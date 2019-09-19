3eacf257cfe143efbad0a90fb4fa466c45345eb6
badai89 <zerobadai5@gmail.com>
2019-09-19 17:18:53 +0700

    express-heroku-changePORT

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
-
-// setting port
-const http = require("http");
-
-const hostname = "127.0.0.1";
-const port = 8888;
-let user = require("./respon-body");
-let router = require("./routes");
-
-const PORT = process.env.PORT || 3000;
-
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader("Content-Type", "text/plain");
-    res.end("hello world ")
-}
-);
-
-server.listen(port, hostname, () => {
-    console.log('server running at http://${127.0.0.1}:${8888}/');
-})
-
-
-
-
 // index.js
 const express = require("express"); // import express library
 const app = express(); // initialize express app
+const PORT = process.env.PORT || 3000;
 
 // setup a `/` endpoint
 app.get("/", (req, res, next) => res.send("Welcome!"));
@@ -34,15 +9,12 @@ app.get("/", (req, res, next) => res.send("Welcome!"));
 // setup a `/hello` endpoint
 app.get("/hello", (request, response, next) => {
   response.send({
-    message: "Hello!", data: user, 
-    
+    message: "Hello!",
+    data: user
   });
 });
 
-
-
 // setup server to listen on port :9999
 app.listen(PORT, () =>
-  console.log("Express server is ready on localhost:3000")
+  console.log(`Express server is ready on port: ${PORT}`)
 );
-
