10d65f246841d8e4fd892cd5eed3e402a542f81c
badai89 <zerobadai5@gmail.com>
2019-09-19 17:24:27 +0700

    express-heroku-setPortheroku

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 const express = require("express"); // import express library
 const app = express(); // initialize express app
 const PORT = process.env.PORT || 3000;
+let user = require("./respon-body");
 
 // setup a `/` endpoint
-app.get("/", (req, res, next) => res.send("Welcome!"));
+app.get("/", (req, res, next) => res.send("Welcome stalker!"));
 
 // setup a `/hello` endpoint
 app.get("/hello", (request, response, next) => {
